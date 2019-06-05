2e80484fdbad26201b2d4e94fab60f2715368bfd
dawnlunacy <alanturing@example.com>
2019-06-05 03:10:02 -0600

    Clean JS, qs for phase two set up

--- main.js
+++ main.js
@@ -34,7 +34,6 @@ taskTitleInput.addEventListener('keyup', btnsHelper);
 makeTaskListBtn.addEventListener('click', makeTaskList);
 
 
-
 function pageLoadHelper() {
   disableMakeTaskListBtn();
   repopulateCardsinfo();
@@ -42,11 +41,9 @@ function pageLoadHelper() {
 
 function repopulateCardsinfo() {
   var existingCards = toDoListArray;
-  console.log("existingInfo", existingCards);
   var freshCardsInfo = existingCards.map(function(cardInfo) {
     return cardInfo = new ToDoList(cardInfo);
   });
-    console.log("reload", freshCardsInfo)
     toDoListArray = freshCardsInfo;
     repopulateCards(toDoListArray);
 };
@@ -58,16 +55,16 @@ function repopulateCards(toDoListArray) {
 };
 
 function previewTaskItemHelper(e) {
-  e.preventDefault()
-  disableAddPreviewTaskBtn()
-  var taskPreviewId = Date.now()
-  var taskPreview = taskItemInput.value
-  appendPreviewTaskItem(taskPreviewId, taskPreview)
+  e.preventDefault();
+  disableAddPreviewTaskBtn();
+  var taskPreviewId = Date.now();
+  var taskPreview = taskItemInput.value;
+  appendPreviewTaskItem(taskPreviewId, taskPreview);
 };
 
 function deletePreviewTaskItemFromDom(e) {
   if(e.target.closest('.preview-checkbox-img')) {
-   e.target.closest('.preview-task-item').remove()
+   e.target.closest('.preview-task-item').remove();
   };
 };
 
@@ -80,12 +77,12 @@ function btnsHelper() {
 function disableAddPreviewTaskBtn() {
   if (taskItemInput.value !== '') {
     addPreviewTaskItemBtn.disabled = false;
-    addPreviewTaskItemBtn.classList.remove('disabled')
-    addPreviewTaskItemBtnImg.classList.remove('disabled')
+    addPreviewTaskItemBtn.classList.remove('disabled');
+    addPreviewTaskItemBtnImg.classList.remove('disabled');
   } else {
     addPreviewTaskItemBtn.disabled = true;
-    addPreviewTaskItemBtn.classList.add('disabled')
-    addPreviewTaskItemBtnImg.classList.add('disabled')
+    addPreviewTaskItemBtn.classList.add('disabled');
+    addPreviewTaskItemBtnImg.classList.add('disabled');
   };
   disableMakeTaskListBtn();
   enableClearAllBtn();
@@ -94,21 +91,21 @@ function disableAddPreviewTaskBtn() {
 function disableMakeTaskListBtn() {
   if (addPreviewTaskItems.innerHTML !== '' && taskTitleInput.value !== '') {
     makeTaskListBtn.disabled = false;
-    makeTaskListBtn.classList.remove('disabled')
+    makeTaskListBtn.classList.remove('disabled');
   } else {
     makeTaskListBtn.disabled = true;
-    makeTaskListBtn.classList.add('disabled')
+    makeTaskListBtn.classList.add('disabled');
   };
 };
 
 function disableClearAllBtn() {
     clearAllBtn.disabled = true;
-    clearAllBtn.classList.add('disabled')
+    clearAllBtn.classList.add('disabled');
 };
 
 function enableClearAllBtn() {
     clearAllBtn.disabled = false;
-    clearAllBtn.classList.remove('disabled')
+    clearAllBtn.classList.remove('disabled');
 };
 
 function clearAllBtnHelper() {
@@ -126,7 +123,7 @@ function makeTaskList() {
 };
 
 function tasksToObjects() {
-  var tasks = document.querySelectorAll('.preview-task-item')
+  var tasks = document.querySelectorAll('.preview-task-item');
   var taskItems = [];
   tasks.forEach(function(task) {
     var taskItem = {
@@ -136,26 +133,23 @@ function tasksToObjects() {
     }
     taskItems.push(taskItem);
   });
-  console.log("item-variable", taskItems);
-  return taskItems
+  return taskItems;
 };
   
 function createNewToDoList() {
   var thisListTitle = taskTitleInput.value;
-  console.log("tiittle", thisListTitle)
   var tasksToAppend = tasksToObjects();
-  console.log("BOYA", tasksToAppend)
   var newToDoList = new ToDoList({
     id: Date.now(),
     title: thisListTitle,
     tasks: tasksToAppend,
     urgency: false
     });
-  console.log("hiiii", newToDoList)
   toDoListArray.push(newToDoList);
-  newToDoList.saveToStorage(toDoListArray)
-  appendToDoListToDom(newToDoList)
+  newToDoList.saveToStorage(toDoListArray);
+  appendToDoListToDom(newToDoList);
 };
+
 function appendTaskToCard(newToDoList) {
   var sortTasksList = '';
   for (var i = 0; i < newToDoList.tasks.length; i++){
@@ -163,12 +157,10 @@ function appendTaskToCard(newToDoList) {
             `<li class="card-task-list" data-id=${newToDoList.tasks[i].id}>
               <img src="images/checkbox.svg" class="card-checkbox-img" alt="empty checkbox">
             ${newToDoList.tasks[i].taskBody}</li>`
-            console.log("TASK", newToDoList.tasks[i].taskBody)
   } return sortTasksList;
 };
 
 function appendToDoListToDom(newToDoList) {
-  console.log("HIYO", toDoListArray)
    var toDoList = 
        `<article class="card-template" id="card-template-urgent" data-id=${newToDoList.id}>
         <h2 class="card-title" id="card-title-urgent">${newToDoList.title}</h2>
@@ -188,7 +180,7 @@ function appendToDoListToDom(newToDoList) {
           </div>
         </footer>
       </article>`
-      cardDisplayArea.insertAdjacentHTML('beforeend', toDoList)
+      cardDisplayArea.insertAdjacentHTML('beforeend', toDoList);
 };
 
 function appendPreviewTaskItem(id, task) {
@@ -201,28 +193,3 @@ function appendPreviewTaskItem(id, task) {
     taskItemInput.value = '';
     disableAddPreviewTaskBtn()
 };
-
-  //Phase Two: Completing The MVP (Minimum Viable Product)
-    //Checking Off A Task 
-      //After a user has completed a task on their checklist, they should be able to check it off
-          //1. There should be a visual cue so that the user knows what they have completed and what is left to do 
-          //2. Tasks that are checked off should persist upon reloading the page. 
-          //3. The update of the data model should occur in the updateTask method that is defined in the ToDoList class
-          //4. How the DOM gets updated using javascript should happen in the main.js file
-
-     //Deleting an Existing ToDo Card
-        // After creating a todo card, the user should be able to remove it once they have completed their checklist. 
-          //1.Each todo card on the todo list should have a button to remove it from both the data model and the DOM
-          //2. The "Delete" button should only be enabled if all of the tasks on the checklist have been checked off
-          //3. Upon clicking the "Delete" button, the appropriate todo list should be removed from the DOM 
-          //4. The update of the data model should happen in the deleteFromStorage method that is defined in the ToDoList class
-          //5. How the DOM gets updated using javascript should happen in the main.js file. 
-
-     // Marking a ToDo Card Urgent
-        // A user should be able to mark their todo cards urgent so that they know which they need to complete first. 
-          //1. When the user clicks on the Urgent button, the button should stay in the active state. 
-          //2. ToDo cards that are marked as urgent should persist upon reloading the page. 
-          //3. This update of the data model should occur in the updateToDo method that is defined in the ToDoList class. 
-          //4. How the DOM gets updated using javascript should happen in the main.js
-
-
